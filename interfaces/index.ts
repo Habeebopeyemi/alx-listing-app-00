1f7c2f34b0e849b703bf55221ac97c5b2fdf794b
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2025-07-20 23:13:37 +0100

    feat: started airbnb layouts

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,7 +1,35 @@
-export interface CardProps{
+export interface CardProps {}
 
+export interface ButtonProps {}
+
+export interface Address {
+  state: string;
+  city: string;
+  country: string;
 }
 
-export interface ButtonProps{
-    
+export interface Offer {
+  bed: string;
+  shower: string;
+  occupants: string;
+}
+export interface PropertyProps {
+  name: string;
+  address: Address;
+  rating: number;
+  category: string[];
+  price: number;
+  offers: Offer;
+  image: string;
+  discount: string;
+}
+
+export interface AccommodationTypes {
+  id?: number;
+  name: string;
+  image: string;
+}
+
+export interface PillProps {
+  title: string;
 }
